00352104a79bc21d7b36731fb11ab0879f0fd4dc
mark-holcomb <77138420+mark-holcomb@users.noreply.github.com>
2022-02-07 20:50:14 -0600

    Add files via upload

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.shoppinglist;
 
+import static com.example.shoppinglist.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+        final Button goToCategories = findViewById(id.categories_btn);
+        final Button goToShoppingLists=findViewById(id.shopping_list_btn);
+
+        View.OnClickListener homePageButtonSelector; // allows any and all buttons on this activity to go through the same OnClickListener()
+        homePageButtonSelector=new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // would use switch-case statement, but resource IDs are non-final, so google advises if-else instead
+                if(view.getId()==id.categories_btn){
+                    Intent main2categories=new Intent(MainActivity.this,CategoriesActivity.class);
+                    startActivity(main2categories);
+                }
+                else if(view.getId()==id.shopping_list_btn){
+                    Intent main2shoppingLists=new Intent(MainActivity.this,ShoppingListsActivity.class);
+                    startActivity(main2shoppingLists);
+                }
+                else {
+                    throw new IllegalStateException("Unexpected value: " + view.getId());
+                }
+            }
+        };
+        goToCategories.setOnClickListener(homePageButtonSelector);
+        goToShoppingLists.setOnClickListener(homePageButtonSelector);
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/ShoppingListsActivity.java
@@ -0,0 +1,14 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_lists);
+    }
+}
